9f2f72c3b7e93fe4f66dde9ea5785256286515cb
Your Name <you@example.com>
2022-05-18 00:39:07 -0300

    m-2

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
-
+import styles from './carta.module.css';
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+  <div className={styles.card}>
+    <button className={styles.boton} onClick={props.onClose}>X</button>
+    <h1>{props.name}</h1>
+    <div className={styles.info}>
+      <div>
+    <p>MIN</p>
+    <p>{props.min}</p>
+    </div>
+    <div>
+    <p>MAX</p>
+    <p>{props.max}</p>
+    </div>
+    <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="no se cargo la img" />
+    </div>
+    </div>
+    )
 };
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-
+import styles from './buscar.module.css';
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return <div className={styles.con}>
+    <input className={styles.inp} type="text" placeholder='Ciudad...' />
+    <button className={styles.bot} onClick={() =>props.onSearch("Buscado Ciudad...")}>Agregar</button>
+  </div>
 };
